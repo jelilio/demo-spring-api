549ac39ae73e3183cd7d60fd9c0460aac9221c85
Jelili Adesina <jelilio.adesina@gmail.com>
2023-07-22 16:10:51 +0100

    initial commit

---
+++ src/main/java/io/github/jelilio/demoapi/DemoApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.jelilio.demoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jelilio/demoapi/DemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jelilio.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
